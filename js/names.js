f10f2b1dffced170640ab1ec0a871c9d9361d0f7
DmaxB <135641740+ItzDmaxB@users.noreply.github.com>
2026-06-29 15:07:42 +0200

    Name api now uses Couldflare worker to reach Ashcons api

--- js/names.js
+++ js/names.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // If the element doesn’t exist on this page → skip it
         if (!nameField) continue;
 
-        fetch(`https://api.ashcon.app/mojang/v2/user/${uuid}`)
+        fetch(`https://mojang-proxy.maxdabe13.workers.dev/?uuid=`)
             .then(res => res.json())
             .then(data => {
                 if (data && data.name) {
