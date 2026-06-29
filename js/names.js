d35fedc8da0f02fd3f468c281ac81cc55a48be23
DmaxB <135641740+ItzDmaxB@users.noreply.github.com>
2026-06-29 14:57:07 +0200

    Changed it so that names contant mojangs api directly

--- js/names.js
+++ js/names.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // If the element doesn’t exist on this page → skip it
         if (!nameField) continue;
 
-        fetch(`https://mojang-proxy.maxdabe13.workers.dev/?uuid=${uuid}`)
+        fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${uuid}`)
             .then(res => res.json())
             .then(data => {
                 if (data && data.name) {
